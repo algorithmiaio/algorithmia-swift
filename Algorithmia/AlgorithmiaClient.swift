4ce35628e0580d3dcf15653cbe5ccbb8c4d4b67f
Erik Ilyin <erik.ilyin@aol.com>
2016-11-01 12:10:54 +0300

    added project

---
+++ Algorithmia/AlgorithmiaClient.swift
@@ -0,0 +1,9 @@
+//
+//  AlgorithmiaClient.swift
+//  AlgorithmiaSwiftDemo
+//
+//  Created by Erik Ilyin on 11/1/16.
+//  Copyright © 2016 algorithmia. All rights reserved.
+//
+
+import Foundation
---
+++ AlgorithmiaSwiftDemo/AlgorithmiaSwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlgorithmiaSwiftDemo
+//
+//  Created by Erik Ilyin on 11/1/16.
+//  Copyright © 2016 algorithmia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
